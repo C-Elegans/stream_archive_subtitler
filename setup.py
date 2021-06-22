4acd936a0852e529afceee257e352c8c6b716fce
Michael Nolan <mtnolan2640@gmail.com>
2021-06-22 13:55:27 -0400

    Bump version, upload to pypi

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='stream_archive_subtitle',
-      version='1.0.3',
+      version='1.0.4',
       packages=['stream_archive_subtitle'],
       #py_modules=['stream_archive_subtitle'],
       author='Michael Nolan',
